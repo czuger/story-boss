0838427c6b704f3e5ac6f043fc6d62f3563de15d
ced <zuger.cedric@gmail.com>
2018-01-27 09:22:46 +0100

    Setting devise ready for ssl.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.to_prepare { Devise::SessionsController.force_ssl }
+  config.to_prepare { Devise::RegistrationsController.force_ssl }
+  config.to_prepare { Devise::PasswordsController.force_ssl }
 end
